91266202c26e5fdfb636791cd14ba3256a8b7764
Beamanator <dabeamanator@gmail.com>
2018-07-16 10:15:12 +0200

    Wrapping routes with layout

--- src/App.js
+++ src/App.js
@@ -5,16 +5,33 @@ import Page1 from './containers/Page1/Page1';
 import Page2 from './containers/Page2/Page2';
 import Home from './containers/Home/Home';
 
+import Layout from './hoc/Layout/Layout';
+
 import './App.css';
 
 class App extends Component {
+    // TODO: Add auto authentication attempts
+    // componentDidMount() {
+    //     this.props.onTryAutoSignin();
+    // }
+
     render() {
-        return (
+        let routes = (
             <Switch>
                 <Route exact path="/" component={Home} />
                 <Route path="/p1" component={Page1} />
                 <Route path="/p2" component={Page2} />
             </Switch>
+        );
+
+        // TODO: add new set of routes if authenticated
+        // -> Useful if we want to restrict admin pages (for example)
+        // if (this.props.isAuthenticated) {...}
+
+        return (
+            <Layout>
+                {routes}
+            </Layout>
         );
     }
 }
