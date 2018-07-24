9606b921a93312484003113d83d4e6507c743ff7
Beamanator <dabeamanator@gmail.com>
2018-07-24 22:10:05 +0200

    reverting to Router since works w/ gh-pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { HashRouter } from 'react-router-dom';
+// import { HashRouter } from 'react-router-dom';
 import { createBrowserHistory } from "history";
 import { Router, Route, Switch } from "react-router";
 
@@ -11,12 +11,12 @@ import "assets/scss/material-kit-react.css?v=1.1.0";
 var hist = createBrowserHistory();
 
 ReactDOM.render(
-  <HashRouter history={hist}>
+  <Router history={hist}>
     <Switch>
       {indexRoutes.map((prop, key) => {
         return <Route path={prop.path} key={key} component={prop.component} />;
       })}
     </Switch>
-  </HashRouter>,
+  </Router>,
   document.getElementById("root")
 );
