1b645b2b91dbcda9fead73b4adf566477ebc5df5
Beamanator <dabeamanator@gmail.com>
2018-07-25 09:26:31 +0200

    adding spacing

--- src/index.js
+++ src/index.js
@@ -11,12 +11,14 @@ import "assets/scss/material-kit-react.css?v=1.1.0";
 var hist = createBrowserHistory();
 
 ReactDOM.render(
-  <HashRouter>
-    <Switch>
-      {indexRoutes.map((prop, key) => {
-        return <Route path={prop.path} key={key} component={prop.component} />;
-      })}
-    </Switch>
-  </HashRouter>,
+    <HashRouter>
+        <Switch>
+            {indexRoutes.map((prop, key) => {
+                return <Route
+                  path={prop.path} key={key} component={prop.component}
+                />;
+            })}
+        </Switch>
+    </HashRouter>,
   document.getElementById("root")
 );
