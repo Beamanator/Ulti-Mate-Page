7689a2f6968359c97936984c1195ef0e682e345c
Beamanator <dabeamanator@gmail.com>
2018-08-21 17:34:55 +0200

    simplifying class declaration

--- src/components/CustomDropdown/CustomDropdown.jsx
+++ src/components/CustomDropdown/CustomDropdown.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 // nodejs library that concatenates classes
 import classNames from "classnames";
 // nodejs library to set properties for components
@@ -19,7 +19,7 @@ import Button from "components/CustomButtons/Button.jsx";
 
 import customDropdownStyle from "assets/jss/material-kit-react/components/customDropdownStyle.jsx";
 
-class CustomDropdown extends React.Component {
+class CustomDropdown extends Component {
   state = {
     open: false
   }
