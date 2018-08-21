f9dc1cdcab1963a23c46736375bdade59adb6cf3
Beamanator <dabeamanator@gmail.com>
2018-08-21 17:35:24 +0200

    commenting out unused imports

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { HashRouter } from 'react-router-dom';
-import { createBrowserHistory } from "history";
-import { Router, Route, Switch } from "react-router";
+// import { createBrowserHistory } from "history";
+import { Route, Switch } from "react-router";
 
 import indexRoutes from "routes/index.jsx";
 
 import "assets/scss/material-kit-react.css?v=1.1.0";
 
-var hist = createBrowserHistory();
+// var hist = createBrowserHistory();
 
 ReactDOM.render(
     <HashRouter>
