5b767dd968de77cbe21684d527ec198d043a6448
Beamanator <dabeamanator@gmail.com>
2018-08-21 17:35:40 +0200

    Adding new home page, moving default home page to /components

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,4 +1,5 @@
 import Components from "views/Components/Components.jsx";
+import Home from 'views/Home/Home.jsx';
 import LandingPage from "views/LandingPage/LandingPage.jsx";
 import ProfilePage from "views/ProfilePage/ProfilePage.jsx";
 import LoginPage from "views/LoginPage/LoginPage.jsx";
@@ -7,7 +8,8 @@ const indexRoutes = [
     { path: "/landing-page", name: "LandingPage", component: LandingPage },
     { path: "/profile-page", name: "ProfilePage", component: ProfilePage },
     { path: "/login-page", name: "LoginPage", component: LoginPage },
-    { path: "/", name: "Components", component: Components }
+    { path: "/components", name: "Components", component: Components },
+    { path: "/", name: "Home", component: Home }
 ];
 
 export default indexRoutes;
